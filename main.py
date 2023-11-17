21282dad1827c57f8446c2dd629b002292a0e06c
home1 <sergnator.de@gmail.com>
2023-11-17 20:04:56 +0300

    doing function

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ class MainWindow(QMainWindow):
         uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.change)
         self.draw_ = False
+        self.setGeometry(100, 100, 1000, 1000)
 
     def change(self):
         self.draw_ = True
+        self.update()
 
     def draw(self, qp: QPainter):
         x, y = random.randint(0, 1000), random.randint(0, 1000)
         center = QPoint(x, y)
-        r = random.randint(1, 100)
-        QPainter.setBrush(QColor(255, 255, 0))
-        QPainter.drawEllipse(center, r)
+        r = random.randint(100, 100)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(center, r, r)
 
     def paintEvent(self, event):
         if self.draw_:
@@ -34,6 +36,11 @@ class MainWindow(QMainWindow):
         self.draw_ = False
 
 
+def except_hook(exc_type, exc_value, exc_tb):
+    print(''.join(traceback.format_exception(exc_type, exc_value, exc_tb)))
+
+
+sys.excepthook = except_hook
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
